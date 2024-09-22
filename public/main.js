af0b6d27c878acbd71d41bc8fb6441f043d5774d
Maksym Zhuk <bitmaksum550@gmail.com>
2024-09-22 18:27:29 +0300

    add login and password verification and notification of successful registration

--- public/main.js
+++ public/main.js
@@ -1,18 +1,45 @@
 $(document).ready(() => {
 
-    $('#submit').click((events) => {
-        alert('hello');
-        events.preventDefault();
+    $('#submit').click((e) => {
+        const correctPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/
+        const correctLogin = /[a-zA-Z0-9]{3,}/
         const login = $('#login').val();
         const password = $('#password').val();
-        axios.post('http://localhost:3000/register', { login, password })
-            .then(res => {
-                console.log(res.data);
-                $('#login').val('');
-                $('#password').val('');
-                alert('User created');
-            }).catch(err => {
-                console.log(err.message);
-            })
+
+        if (correctLogin.test(login)) {
+            $(`.loginRequirementsContainer`).text('');
+            if (correctPassword.test(password)) {
+                $(`.passwordRequirementsContainer`).text('');
+                e.preventDefault();
+                axios.post('http://localhost:3000/register', { login, password })
+                    .then(res => {
+                        console.log(res.data);
+                        $('#login').val('');
+                        $('#password').val('');
+                        $(`.registerContainer`).css(`display`, `none`);
+                        $(`.notificationPopup`).css(`display`, `flex`);
+                        $(`.notificationPopup`).css(`animation`, `2s ease forwards draw`);
+                        setTimeout(() => {
+                            $(`.notificationPopup`).css(`animation`, `none`);
+                            $(`.notificationPopup`).css(`display`, `none`);
+                            $(`.registerContainer`).css(`display`, `flex`);
+                            $(`.animate`).css(`display`, `flex`);
+                        }, 2000);
+                    }).catch(err => {
+                        console.log(err.message);
+                    })
+            } else {
+                $(`.passwordRequirementsContainer`).text(`Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character.`);
+            }
+        } else {
+            $(`.loginRequirementsContainer`).text(`Login must be at least 3 characters long and contain only letters and numbers.`);
+        }
+    })
+
+    $(`#login`).focus(() => {
+        $(`.passwordRequirementsContainer`).text('');
+    })
+    $(`#password`).focus(() => {
+        $(`.loginRequirementsContainer`).text('');
     })
 })
